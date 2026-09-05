3628bd03ccd5e9a92f3c1e4c86e1040c8deb26b1
fast <cesarcossio@gmail.com>
2026-09-05 02:52:13 -0500

    The "shared" code is added for future projects.

--- LogicConcepts/IsNumberOddOrNot/Program.cs
+++ LogicConcepts/IsNumberOddOrNot/Program.cs
@@ -1,30 +1,31 @@
-﻿var numberString = string.Empty;
+﻿using Shared;
+
+var answer = string.Empty;
+var options = new List<string> { "s", "n" };
+
 do
 {
-    Console.Write("Ingrese numero entero o la palabra 'salir' para salir: ");
-    numberString = Console.ReadLine();
-    if (numberString!.ToLower() == "salir")
+    var number = ConsoleExtension.GetInt("Ingrese número entero diferente de cero: ");
+    if (number == 0)
     {
         continue;
     }
-
-    var numberInt = 0;
-    if (int.TryParse(numberString, out numberInt))
-    {
-        if (numberInt % 2 == 0)
+            
+    if (number % 2 == 0)
         {
-            Console.WriteLine($"El número {numberInt}, es par.");
+            Console.WriteLine($"El número {number}, es par.");
         }
-        else
-        {
-            Console.WriteLine($"El número {numberInt}, es impar.");
-        }
-    }
     else
+        {
+            Console.WriteLine($"El número {number}, es impar.");
+        }
+
+    do
     {
-        Console.WriteLine($"Lo que ingresaste {numberString}, no es un número entero.");
-    }
-} while (numberString!.ToLower() != "salir");
+        answer = ConsoleExtension.GetValidOptions("¿Deseas continuar [S]í, [N]o?: ", options);
+    } while (!options.Any(x => x.Equals(answer, StringComparison.CurrentCultureIgnoreCase)));
+
+} while (answer!.Equals("s", StringComparison.CurrentCultureIgnoreCase));
 Console.WriteLine("Game Over.");
     
 
