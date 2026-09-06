01bc12ccda881d1b33193d68a2d961f88a2bd994
fast <cesarcossio@gmail.com>
2026-09-05 22:33:55 -0500

    Add 4to exercise "IsMultipleOfAnother"

---
+++ LogicConcepts/IsMultilpeOfAnother/Program.cs
@@ -0,0 +1,25 @@
+﻿using Shared;
+
+var answer = string.Empty;
+var options = new List<string> { "s", "n" };
+
+do
+{
+    var a = ConsoleExtension.GetInt("Ingrese primer número : ");
+    var b = ConsoleExtension.GetInt("Ingrese segundo número: ");
+
+    if (b % a == 0 )
+    {
+        Console.WriteLine($"El número {a}, es multiplo de {b}");   
+    }
+    else
+    {
+        Console.WriteLine($"El número {a}, NO es multiplo de {b}");
+    }
+    do
+    {
+        answer = ConsoleExtension.GetValidOptions("¿Deseas continuar [S]í, [N]o?: ", options);
+    } while (!options.Any(x => x.Equals(answer, StringComparison.CurrentCultureIgnoreCase)));
+
+} while (answer!.Equals("s", StringComparison.CurrentCultureIgnoreCase));
+Console.WriteLine("Game Over.");
