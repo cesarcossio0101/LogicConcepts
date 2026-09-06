25a11e4efe1293321cf063d19516cdaa0789c4e4
fast <cesarcossio@gmail.com>
2026-09-06 04:32:15 -0500

    The final class "Trapeze" has been created, and the console output—which is currently disorganized—still needs to be arranged.

---
+++ LogicConcepts/Backend/Trapeze.cs
@@ -0,0 +1,45 @@
+﻿namespace Backend;
+
+public class Trapeze : Triangle
+{
+
+    // Fields
+
+    private double _d;
+
+    // Constructors
+
+    public Trapeze(string name, double a, double b, double c, double h, double d)
+        : base(name, a, b, c, h)
+    {
+        D = d;
+    }
+
+
+    // Properties
+
+    public double D 
+    { 
+        get => _d; 
+        set => _d = value; 
+    }
+
+    // Public methods
+
+    public override double GetArea() => (B + D) * H / 2;
+
+    public override double GetPerimeter() => A + B + C + D;
+
+    // Private methods
+
+    private double ValidateD(double D)
+    {
+        if (D <= 0)
+        {
+            throw new Exception($"The {D} must be greater than zero.");
+        }
+        return D;
+
+    }
+
+}
